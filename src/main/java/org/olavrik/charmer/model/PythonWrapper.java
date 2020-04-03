21d38eaf14f12747b2d35e4791416d2fbc488812
Olga Lavrichenko <lavrikolyalavrik@gmail.com>
2020-04-03 16:04:55 +0300

    Added class for handling  python interactive mode - PythonWrapper + PythonWrapperTest

--- src/main/java/org/olavrik/charmer/model/PythonWrapper.java
+++ src/main/java/org/olavrik/charmer/model/PythonWrapper.java
@@ -1,4 +1,104 @@
 package org.olavrik.charmer.model;
 
+import java.io.*;
+import java.util.ArrayList;
+
+
 public class PythonWrapper {
+    ProcessBuilder processBuilder;
+    String pythonPath;
+    Process process;
+    BufferedWriter bufferedWriter;
+
+    public PythonWrapper() {
+
+    }
+
+    public void setPythonPath(String pythonPath) {
+        this.pythonPath = (pythonPath == null) ? "/usr/local/bin/python3" : pythonPath;
+    }
+
+    public String getPythonPath() {
+        return pythonPath;
+    }
+
+
+    private ArrayList<String> readOutput() throws IOException, InterruptedException {
+        BufferedReader outputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        ArrayList<String> dataOutput = new ArrayList<String>();
+        while (!outputReader.ready()) {
+            Thread.sleep(100);
+        }
+        String line;
+
+        while (outputReader.ready() && (line = outputReader.readLine()) != null) {
+            dataOutput.add(line);
+        }
+        return dataOutput;
+    }
+
+
+    public Boolean checkPython() throws  InterruptedException {
+        try {
+
+
+            ProcessBuilder checkpython = new ProcessBuilder(new String[]{this.pythonPath, "-i", "--version"});
+            Process check = checkpython.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(check.getInputStream()));
+            BufferedReader readererror = new BufferedReader(new InputStreamReader(check.getErrorStream()));
+
+            while (!reader.ready() && !readererror.ready()) {
+                Thread.sleep(100);
+            }
+
+            if (readererror.ready()) {
+                return false;
+            }
+
+            String line;
+            if (reader.ready() && (line = reader.readLine()) != null) {
+                if (line.indexOf("Python") != -1 && line.indexOf("3.") != -1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+        }catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
+
+
+    }
+
+
+    public void startProcess() throws IOException {
+        this.processBuilder = new ProcessBuilder(new String[]{this.pythonPath, "-i"});
+        this.process = this.processBuilder.start();
+
+    }
+
+    public ArrayList<String> runCmd(String[] command, Boolean flag) {
+        try {
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+            for (int i = 0; i < command.length; i++) {
+                this.bufferedWriter.write(command[i]);
+                this.bufferedWriter.newLine();
+            }
+            this.bufferedWriter.flush();
+            if (flag) {
+                return readOutput();
+            }
+
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
---
+++ src/test/java/org/olavrik/charmer/PythonWrapperTest.java
@@ -0,0 +1,45 @@
+package org.olavrik.charmer;
+
+import org.junit.*;
+import org.olavrik.charmer.model.PythonWrapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class PythonWrapperTest {
+    private PythonWrapper pythonWrapper;
+
+    @Before
+    public void initTest() throws IOException {
+        this.pythonWrapper = new PythonWrapper();
+        this.pythonWrapper.setPythonPath(System.getenv("PYTHON_PATH"));
+        this.pythonWrapper.startProcess();
+    }
+
+    @Test
+    public void checkPythonSum(){
+        ArrayList<String> actual=this.pythonWrapper.runCmd(new String[]{"2+2"}, true);
+        assertEquals("4", actual.get(0));
+    }
+
+    @Test
+    public void checkPythonPrint(){
+        ArrayList<String> actual=this.pythonWrapper.runCmd(new String[]{"print(\"meow\")"}, true);
+        assertEquals("meow", actual.get(0));
+    }
+
+    @Test
+    public void checkPandas(){
+        String[] commands=new String[]{"import pandas as pd", "print (pd.Series([1])[0])"};
+        ArrayList<String> actual=this.pythonWrapper.runCmd(commands, true);
+        assertEquals("1",actual.get(0));
+    }
+
+
+    @After
+    public void afterTest() {
+        pythonWrapper = null;
+    }
+}
